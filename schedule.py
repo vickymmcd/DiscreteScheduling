b9cd54ebc5f6e605ad4ac6e185b2cb0d3f0ce607
vickymmcd <vickymmcd@gmail.com>
2018-11-26 22:19:34 -0500

    starting to fix recursion problems

--- schedule.py
+++ schedule.py
@@ -1,27 +1,26 @@
 def schedule(graph, colors):
     result = []
+    colored = 0
     for i in range(0, len(graph[0])):
         result.append(-1)
 
     # consider the "name" of each vertex to be its index where the first one is 0
-    def check_vertex(vertex):
-        print(vertex)
-        print(len(graph[0]))
+    def check_vertex(vertex, colored):
         if vertex >= len(graph[0]):
-            print("hiya!!")
             return
-            print("this shouldn't happen")
 
         for color in colors:
-            print(vertex, color)
-            print("but this does??")
-            if (is_possible(vertex, color, graph, result)):
+            if vertex >= len(graph[0]):
+                return
+            print(vertex, color, colored)
+            if (colored < len(graph[0]) and is_possible(vertex, color, graph, result)):
                 result[vertex] = color
+                colored+=1
                 print("possible")
                 vertex = vertex + 1
-                check_vertex(vertex)
+                check_vertex(vertex, colored)
 
-    check_vertex(0)
+    check_vertex(0, colored)
     return result
 
 
