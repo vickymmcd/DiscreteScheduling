164c20bb083c0e9cfedf3f7edc68e9a54382aa38
vickymmcd <vickymmcd@gmail.com>
2018-11-28 08:55:57 -0500

    making schedule into a python class and adding both student and instructor matrices

--- schedule.py
+++ schedule.py
@@ -1,34 +1,45 @@
-def schedule(graph, colors):
-    result = []
-    colored = 0
-    for i in range(0, len(graph[0])):
-        result.append(-1)
+class Schedule:
+    def __init__(self, instructor_matrix, student_matrix, colors, num_classes):
+        self.instructors = instructor_matrix
+        self.students = student_matrix
+        self.colors = colors
+        self.num_classes = num_classes
 
-    # consider the "name" of each vertex to be its index where the first one is 0
-    def check_vertex(vertex, colored):
-        if vertex >= len(graph[0]):
-            return
+    def get_coloring(self):
+        self.result = []
+        colored = 0
+        for i in range(0, self.num_classes):
+            self.result.append(-1)
 
-        for color in colors:
-            if vertex >= len(graph[0]):
-                return
-            if (colored < len(graph[0]) and is_possible(vertex, color, graph, result)):
-                result[vertex] = color
-                colored+=1
-                vertex = vertex + 1
-                check_vertex(vertex, colored)
+        #TODO (Vicky): Do this in order of descending conflict weight, sort first or something
+        # consider the "name" of each vertex to be its index where the first one is 0
+        def check_vertex(vertex, colored):
+            if vertex >= self.num_classes:
                 return
 
-    check_vertex(0, colored)
-    return result
+            for color in self.colors:
+                if vertex >= self.num_classes:
+                    return
+                if (colored < self.num_classes and self.is_possible(vertex, color)):
+                    self.result[vertex] = color
+                    colored+=1
+                    vertex = vertex + 1
+                    check_vertex(vertex, colored)
+                    return
 
+        check_vertex(0, colored)
+        return self.result
 
-def is_possible(vertex, color, graph, result):
-    for v in range(0, len(graph[0])):
-        if (graph[v][vertex] >= 1 and result[v] == color):
+    def is_possible(self, vertex, color):
+        for v in range(0, self.num_classes):
+            if (self.instructors[v][vertex] >= 1 and self.result[v] == color):
                 return False
-    return True
+            if (self.students[v][vertex] >= 3 and self.result[v] == color):
+                return False
+        return True
 
-res1 = schedule([[0,1,0],[1,0,0],[0,0,0]], [0,1,2])
+
+schedule = Schedule([[0,1,0],[1,0,0],[0,0,0]], [[0,1,0],[1,0,0],[0,0,0]], [0,1,2], 3)
+res1 = schedule.get_coloring()
 for val in res1:
     print(val)
